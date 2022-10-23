6cee809ab9391a9ac2f87ec6a2e3b2f6c8d985ea
Timothy Hoyt <timothy.hoyt@alertinnovation.com>
2022-10-23 08:51:21 -0500

    food

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-const sqrt = Math.sqrt;
+const sqrt = Math.sqrt; const random = Math.random; const sin = Math.sin; const cos = Math.cos;
 
 //Main Animation Loop
 const mainLoop = function(timeStamp){
@@ -9,22 +9,36 @@ const mainLoop = function(timeStamp){
     ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.fillRect(0, 0, cWidth, cHeight);
 
+
+    if(firstFrame){
+        for(var i=0; i<50; i++){
+            food.get('new')();
+        }
+        firstFrame = false;
+    }
+
+
     //draw
-    someRect.get('draw')();
+    
+    food.forEach((val, key)=>{
+        if(!isNaN(key)){
+            food.get(key).get("draw")();
+        }
+    })
+
+    // someRect.get('draw')();
     someCir.get('draw')();
 
 
     //calcs
-    // if(someRect.get('pos')[0]>0){
-    //     moveShape(someRect, 'x', -0.1)
-    // }
-
-    // if(someCir.get('pos')[0]>0){
-    //     moveShape(someCir, 'x', -0.1)
-    // }
-    someRect.get('calc')();
+    // someRect.get('calc')();
     someCir.get('calc')();
 
+
+
+
+
+    /////////////////////////////////////////necessary
     //cursor
     fillCirRel([mX, mY, 0.5], 'rgba(255,255,255,0.5)')
 
@@ -46,14 +60,21 @@ const moveShape = function(theShape, dir, howmuch){
 
 const moveShapeByVec = function(theShape, theVec){
     const pos = theShape.get('pos')
-    pos[0]+=theVec[0]
-    pos[1]+=theVec[1]
+    pos[0]+=theVec[0];  pos[1]+=theVec[1]
     theShape.set('pos', pos)
 }
 
-const vecDiff = function(vecTo, vecFrom){
-    return [ vecTo[0]-vecFrom[0] , vecTo[1]-vecFrom[1] ]
+const stepShapeToDest = function(theShape){
+    const diffVec = vecDiff(theShape.get('dest'), theShape.get('pos'))
+    const len = vecLen(diffVec);  const speed = theShape.get('speed')
+    const ratio = speed/len
+    if(len>0){
+        if(len >= speed){  moveShapeByVec(theShape, [ratio*diffVec[0], ratio*diffVec[1] ])}
+        else{ moveShapeByVec(theShape, diffVec)}
+    }
 }
+
+const vecDiff = function(vecTo, vecFrom){return [ vecTo[0]-vecFrom[0] , vecTo[1]-vecFrom[1] ]}
 
 onkeydown = (e) =>{
     // console.log(e.key)
@@ -63,18 +84,11 @@ const mouseOverRect = function(rect){
     return mX >= rect[0] && mX <= (rect[0]+rect[2]) && mY >= rect[1] && mY <= (rect[1]+rect[3])
 }
 
-const mouseOverCir = function(cir){
-    const ret = dist([mX,mY], cir) < cir[2];
-    return ret;
-}
+const mouseOverCir = function(cir){ return dist([mX,mY], cir) < cir[2];}
 
-const dist = function(P1, P2){
-    return sqrt((P2[0]-P1[0])**2 + (P2[1]-P1[1])**2);
-}
+const dist = function(P1, P2){ return sqrt((P2[0]-P1[0])**2 + (P2[1]-P1[1])**2);}
 
-const vecLen = function(theVec){
-    return dist(theVec, [0,0])
-}
+const vecLen = function(theVec){return dist(theVec, [0,0])}
 
 onmousedown = (e) => {
     if(e.button === 0){
@@ -87,10 +101,7 @@ onmousedown = (e) => {
     }
 }
 
-const onMouseDownCanvas = function(){
-    console.log("Canvas Clicked:",mdX, mdY);
-    mouseDownCan = true;
-}
+const onMouseDownCanvas = function(){ console.log("Canvas Clicked:",mdX, mdY); mouseDownCan = true;}
 
 onmouseup = (e) => {
     if(mouseDownCan){
@@ -99,8 +110,7 @@ onmouseup = (e) => {
         muX = 100*(mcuX - brect.left)/cW; muY = 100*(mcuY - brect.top)/cH;
         muX = (muX < 0)? 0 : muX;  muX = (muX > 100)? 100 : muX;
         muY = (muY < 0)? 0 : muY;  muY = (muY > 100)? 100 : muY;
-        mouseDownCan = false;
-        console.log("Canvas Unclick:",muX,muY)
+        mouseDownCan = false;  console.log("Canvas Unclick:",muX,muY)
     }
 }
 
@@ -113,81 +123,62 @@ onmousemove = (e) => {
     mY = (mY < 0)? 0 : mY;  mY = (mY > 100)? 100 : mY;
 }
 
-const readWindowSize = function(){
-    wWidth = window.innerWidth; wHeight = window.innerHeight;
-}
+const readWindowSize = function(){ wWidth = window.innerWidth; wHeight = window.innerHeight;}
 
 const setStyles = function(){
     //default (normal horizontal)
     const menuMin = 0.3;
     var marg, canH, canW, canT, menH, menW, menL
-    marg = (wHeight*0.01);
-    canH = (wHeight-marg*2); canW = canH;
-    canT = marg; canL = marg;
-    menW = (wWidth - canW - marg*3); menH = wHeight - marg*2;
-    menL = canW + marg*2;
+    marg = (wHeight*0.01); canH = (wHeight-marg*2); canW = canH;
+    canT = marg; canL = marg; menW = (wWidth - canW - marg*3); 
+    menH = wHeight - marg*2; menL = canW + marg*2;
 
     if(orien == "horizontal"){
         if(wWidth < (wHeight + marg + menuMin*wWidth)){
             // horizontal squeeze
             menW = wWidth*menuMin; canW = wWidth - menW - marg*3;
-            canH = canW; menL = canW + marg*2;
-            canT = wHeight/2 - canH/2;
+            canH = canW; menL = canW + marg*2; canT = wHeight/2 - canH/2;
         }
     }else{
         //vertical
         if(wHeight > (wWidth + marg + menuMin*wHeight)){
             //normal
-            canW = (wWidth-marg*2); canH = canW;
-            menH = (wHeight - canH - marg*3);
-            menW = wWidth - marg*2;  menL = marg;
-            canT = menH + marg*2;
+            canW = (wWidth-marg*2); canH = canW;  menH = (wHeight - canH - marg*3);
+            menW = wWidth - marg*2;  menL = marg; canT = menH + marg*2;
         }else{
             //squeeze
-            menH = wHeight*menuMin;
-            menW = wWidth - marg*2;  menL = marg;
-            canH = wHeight - menH - marg*3;
-            canW = canH;  canT = menH + marg*2;
-            canL = wWidth/2 - canW/2;
+            menH = wHeight*menuMin; menW = wWidth - marg*2;  menL = marg;
+            canH = wHeight - menH - marg*3; canW = canH;  
+            canT = menH + marg*2; canL = wWidth/2 - canW/2;
         }
     }
 
-    canvas.style.width = canW.toString()+"px";
-    canvas.style.height = canH.toString()+"px";
-    canvas.style.marginLeft = canL.toString()+"px";
-    canvas.style.marginTop = canT.toString()+"px";
-    canvas.style.marginRight = marg.toString()+"px";
-    menudiv.style.left = menL.toString()+"px";
-    menudiv.style.width = menW.toString()+"px";
-    menudiv.style.height = menH.toString()+"px";
-    menudiv.style.marginTop = marg.toString()+"px";
+    canvas.style.width = canW.toString()+"px";  canvas.style.height = canH.toString()+"px";
+    canvas.style.marginLeft = canL.toString()+"px"; canvas.style.marginTop = canT.toString()+"px";
+    canvas.style.marginRight = marg.toString()+"px";  
+    menudiv.style.left = menL.toString()+"px"; menudiv.style.width = menW.toString()+"px";  
+    menudiv.style.height = menH.toString()+"px"; menudiv.style.marginTop = marg.toString()+"px";
 }
 
 const checkorien = function(){
-    const prevorien = orien;
     if(wWidth >= wHeight) {orien = "horizontal"} else {orien = "vertical"}; setStyles()
 }
 
 const sizeCanvas = function(){
     readWindowSize(); checkorien(); 
-    brect = canvas.getBoundingClientRect();
-    dvp = window.devicePixelRatio || 1;
-    cWidth = (brect.right - brect.left) * dvp;
-    cHeight = (brect.bottom - brect.top) * dvp; 
-    canvas.width = cWidth; canvas.height = cHeight;
-    cAR = cWidth/cHeight
+    brect = canvas.getBoundingClientRect(); dvp = window.devicePixelRatio || 1;
+    cWidth = (brect.right - brect.left) * dvp; cHeight = (brect.bottom - brect.top) * dvp; 
+    canvas.width = cWidth; canvas.height = cHeight; cAR = cWidth/cHeight
 }
 
 const fillRectRel = function(rect,c){
-    ctx.fillStyle = c;
-    ctx.fillRect(rect[0]/100*cWidth,rect[1]/100*cHeight,rect[2]/100*cWidth,rect[3]/100*cHeight);
+    ctx.fillStyle = c; ctx.fillRect(rect[0]/100*cWidth,rect[1]/100*cHeight,rect[2]/100*cWidth,rect[3]/100*cHeight);
 }
 
 const fillCirRel = function (cir,c){
     ctx.beginPath();
     ctx.arc(cir[0]/100*cWidth, cir[1]/100*cHeight, cir[2]/100*cHeight, 0, 2 * Math.PI, false);
-    ctx.fillStyle = c;  ctx.fill(); ctx.lineWidth = 1;
-    ctx.strokeStyle = 'rgba(0,0,0,0)';  ctx.stroke();
+    ctx.fillStyle = c;  ctx.fill(); ctx.lineWidth = 1; ctx.strokeStyle = 'rgba(0,0,0,0)';  ctx.stroke();
 }
 
 
@@ -196,53 +187,40 @@ const fillCirRel = function (cir,c){
 ////////////////
 
 //elements
-const maindiv = document.getElementsByTagName('main')[0];
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext("2d");
-const menudiv = document.getElementById('theMenu')
+const maindiv = document.getElementsByTagName('main')[0]; const canvas = document.createElement('canvas');
+const ctx = canvas.getContext("2d"); const menudiv = document.getElementById('theMenu')
 
 //constants
 const CC = [ 'rgba(100,200,255,0.50)', 'rgba(100,100,255,0.50)', 'rgba(100,50,200,0.50)', 'rgba(50,100,255,0.50)'
     ,'rgba(100,200,255,0.25)', 'rgba(100,100,255,0.25)', 'rgba(100,50,200,0.25)', 'rgba(50,100,255,0.25)']
 
-
 //variables
 var brect, dvp, cAR, wWidth, wHeight, cWidth, cHeight, mcX, mcY, mX, mY, mcdX, mcdY, mdX, mdY, mcuX, mcuY, muX, muY;
-var mouseDownCan = false; var orien = "horizontal";
+var mouseDownCan = false; var orien = "horizontal"; var firstFrame = true;
 
-//maps
-const someRect = new Map();
-someRect.set('pos',[50,50])
-someRect.set('size', [25,25])
-someRect.set('dest', [0,10])
-someRect.set('speed', 0.1);
-someRect.set('name', 'someRect')
-someRect.set('C', [CC[0], CC[1], CC[2], CC[3]])
-someRect.set('draw', ()=>{
-    const C = someRect.get('C')
-    const rec1 = [someRect.get('pos')[0], someRect.get('pos')[1], someRect.get('size')[0], someRect.get('size')[1]]
-    var rectC = mouseDownCan? mouseOverRect(rec1)? C[0] : C[1] : mouseOverRect(rec1)? C[2] : C[3]
-    fillRectRel(rec1, rectC);
+
+
+const food = new Map();
+food.set("last", 0);
+food.set("new", ()=>{
+    
+    var num = food.get('last');
+    const newFood = new Map();
+    newFood.set('num', num);
+    newFood.set('pos',[random()*(80)+10,random()*(80)+10]);
+    newFood.set('size', [3,3]);
+    newFood.set('C', [CC[0], CC[1], CC[2]]);
+    newFood.set('draw', ()=>{
+        const C = newFood.get('C');
+        const rec1 = [newFood.get('pos')[0], newFood.get('pos')[1], newFood.get('size')[0], newFood.get('size')[1]];
+        var rectC = mouseDownCan? mouseOverRect(rec1)? C[0] : C[1] : mouseOverRect(rec1)? C[2] : C[1];
+        fillRectRel(rec1, rectC);
+        
+    })
+    food.set(num, newFood);
+    food.set('last', num+1);
 })
-someRect.set('calc', ()=>{
-    const diffVec = vecDiff(someRect.get('dest'), someRect.get('pos'))
-    const len = vecLen(diffVec)
-    const speed = someRect.get('speed')
-    const ratio = speed/len
-    if(len>0){
-        if(len >= speed){
-            moveShapeByVec(someRect, [ratio*diffVec[0], ratio*diffVec[1] ])
-        }else{
-            moveShapeByVec(someRect, diffVec)
-        }
-    }
-    // moveShapeByVec(someRect, )
-})
-// someRect.set('move',(dir, howmuch) => {
-//     const pos = someRect.get('pos')
-//     if(dir === 'x'|| dir === 'X'){ pos[0]+=howmuch; }
-//     else if(dir === 'y' || dir === 'Y'){ pos[1]+=howmuch; }
-//     someRect.set('pos', pos)
+
 
 
 const someCir = new Map();
@@ -251,24 +229,18 @@ someCir.set('rad', 10)
 someCir.set('dest', [100,10])
 someCir.set('speed', 0.2)
 someCir.set('name', 'someCir')
-someCir.set('C', [CC[4], CC[5], CC[6], CC[7]])
+someCir.set('C', [CC[4], CC[5], CC[6]])
 someCir.set('draw',()=>{
     const C = someCir.get('C')
     const cir1 = [someCir.get('pos')[0], someCir.get('pos')[1], someCir.get('rad')]
-    console.log(cir1)
-    var cirC = mouseDownCan? mouseOverCir(cir1)? C[0] : C[1] : mouseOverCir(cir1)? C[2] : C[3]
+    // console.log(cir1)
+    var cirC = mouseDownCan? mouseOverCir(cir1)? C[0] : C[1] : mouseOverCir(cir1)? C[2] : C[1]
     fillCirRel(cir1, cirC);
 })
 someCir.set('calc', ()=>{
-    
+    stepShapeToDest(someCir)
 })
 
-// someCir.set('move',(dir, howmuch) => {
-//     const pos = someCir.get('pos')
-//     if(dir === 'x'|| dir === 'X'){ pos[0]+=howmuch;}
-//     else if(dir === 'y' || dir === 'Y'){ pos[1]+=howmuch;}
-//     someCir.set('pos', pos)
-// })
 
 
 ////////////////
